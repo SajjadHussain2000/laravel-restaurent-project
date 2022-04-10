89303392a160a8bfa39306c6a220ac34237abd7b
SajjadHussain2000 <sh267002@gmail.com>
2022-04-10 18:22:49 +0530

    adding login logout on homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -66,22 +66,16 @@
                         -->
                             <li class="scroll-to-section"><a href="#menu">Menu</a></li>
                             <li class="scroll-to-section"><a href="#chefs">Chefs</a></li> 
-                            <li class="submenu">
-                                <a href="javascript:;">Features</a>
-                                <ul>
-                                    <li><a href="#">Features Page 1</a></li>
-                                    <li><a href="#">Features Page 2</a></li>
-                                    <li><a href="#">Features Page 3</a></li>
-                                    <li><a href="#">Features Page 4</a></li>
-                                </ul>
-                            </li>
                             <!-- <li class=""><a rel="sponsored" href="https://templatemo.com" target="_blank">External URL</a></li> -->
                             <li class="scroll-to-section"><a href="#reservation">Contact Us</a></li> 
                             <li>
                             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                       <li><x-app-layout>
+  
+                        </x-app-layout>
+                        </li>
                     @else
                         <li><a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a></li>
 
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get("/home",[HomeController::class,"index"]);
+Route::get("/",[HomeController::class,"index"]);
 
 Route::middleware([
     'auth:sanctum',
